fad3757169262b225bc242c074e8b603e1ad9438
Héctor González M. <hectorgonzalezmero95@gmail.com>
2025-02-03 22:18:06 -0500

    Creacion de BLs

--- src/BusinessLogic/UsuarioBL.java
+++ src/BusinessLogic/UsuarioBL.java
@@ -1,5 +1,37 @@
 package BusinessLogic;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+import DataAccess.DTO.UsuarioDTO;
+import DataAccess.DAO.UsuarioDAO;
+
 public class UsuarioBL {
-    
+    private UsuarioDAO uDAO ;
+    private UsuarioDTO Usuario; //cache
+
+    public UsuarioBL() {
+        uDAO = new UsuarioDAO();
+    }
+
+    public List<UsuarioDTO> getAll() throws Exception{
+        return uDAO.readAll();
+    } 
+
+    public UsuarioDTO getByIdUsuario(int idUsuario) throws Exception {
+        Usuario = uDAO.readBy(idUsuario);
+        return Usuario;
+    }
+
+    public boolean create(UsuarioDTO usuarioDTO) throws Exception {
+        return uDAO.create(usuarioDTO);
+    }
+
+    public boolean update(UsuarioDTO usuarioDTO) throws Exception {
+        return uDAO.update(usuarioDTO);
+    }
+
+    public boolean delete(int idUsuario) throws Exception {
+        return uDAO.delete(idUsuario);
+    }
 }
