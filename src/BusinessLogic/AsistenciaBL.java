7036cfc0e7e02dd8d999055200a7e6f8545199ec
Héctor González M. <hectorgonzalezmero95@gmail.com>
2025-02-04 13:12:48 -0500

    bl Asistencia

--- src/BusinessLogic/AsistenciaBL.java
+++ src/BusinessLogic/AsistenciaBL.java
@@ -1,5 +1,38 @@
 package BusinessLogic;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+import DataAccess.DAO.AsistenciaDAO;
+import DataAccess.DTO.AsistenciaDTO; 
+
 public class AsistenciaBL {
+    private AsistenciaDTO Asistencia; //cache
+    private AsistenciaDAO aDAO = new AsistenciaDAO();
+
+
+        public boolean registrarAsistencia(AsistenciaDTO asistencia) throws Exception {
+        if (asistencia.getFecha() == null) {
+            throw new IllegalArgumentException("La fecha de asistencia no puede ser nula.");
+        }
+        return aDAO.insert(asistencia); 
+    }
     
-}
+        public List<AsistenciaDTO> getAll() throws Exception {
+            return aDAO.readAll();
+        }
+    
+        public AsistenciaDTO getById(int idAsistencia) throws Exception {
+            Asistencia = aDAO.readBy(idAsistencia);
+            return Asistencia;
+        }
+    
+        public boolean update(AsistenciaDTO Asistencia) throws Exception {
+            return aDAO.update(Asistencia);
+        }
+    
+        public boolean delete(int idAsistencia) throws Exception {
+            return aDAO.delete(idAsistencia);
+        }
+    }
+    
