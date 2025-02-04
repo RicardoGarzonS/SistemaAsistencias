02dbb6f68fd06b3baf7f4e79c3dc9ad2f2675c86
Amsi <sebas.jibajava@gmail.com>
2025-02-04 13:10:50 -0500

    lectura de la tarjeta

--- src/BusinessLogic/Entities/Lector.java
+++ src/BusinessLogic/Entities/Lector.java
@@ -1,5 +1,19 @@
 package BusinessLogic.Entities;
 
+import java.util.Scanner;
+
 public class Lector {
+
+    protected int lecturaTarjeta (){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("(No se si borrarlo) Ingrese la tarjeta");
+        String stringTarjeta = sc.nextLine();
+        int retorno = Integer.parseInt(stringTarjeta);
+        sc.close();
+        return retorno;
+
+    }
+
     
 }
