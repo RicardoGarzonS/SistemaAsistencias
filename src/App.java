fd06df107ac311b387c9597f17a0323c7561e2d0
Amsi <sebas.jibajava@gmail.com>
2025-02-04 19:11:57 -0500

    arreglo comit

--- src/App.java
+++ src/App.java
@@ -1,11 +1,7 @@
-import BusinessLogic.Entities.Login;
+
 
 public class App {    
     public static void main(String[] args) throws Exception {
-        
-        Login login = new Login();
-        
-        login.loginCuenta("correojuan", "1234");
     
     }
 }
