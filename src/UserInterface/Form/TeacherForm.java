eab52f0e7929fa95cde5fbe64b0516e96c841ab3
RicardoGarzonS <ricardogarzon2045@outlook.com>
2025-02-08 20:02:43 -0500

    Agregar panel para profesor

--- src/UserInterface/Form/TeacherForm.java
+++ src/UserInterface/Form/TeacherForm.java
@@ -20,51 +20,67 @@ public class TeacherForm extends JFrame{
     private UsuarioDTO  usuario;
     private Docente     docente;
     private MainForm    padre;
-    final   int         ESCALA          = 70;
-    final   int         ALTO            = ESCALA * 9;
-    final   int         ANCHO           = ESCALA * 16;
-    final   int         SEPARACION      = ALTO / 25;
+    final   int         ESCALA              = 70;
+    final   int         ALTO                = ESCALA * 9;
+    final   int         ANCHO               = ESCALA * 16;
+    final   int         SEPARACION          = ALTO / 25;
 
-    private JPanel      cabecera        = new JPanel();
-    private JLabel      bienvenida      = new JLabel();
-    private JPanel      navegacion      = new JPanel();
-    private G3Button    logoutBtn       = new G3Button("CERRAR SESION");
-    private G3Button    backBtn         = new G3Button("INICIO");
+    private JPanel      cabecera            = new JPanel();
+    private JLabel      bienvenida          = new JLabel();
+    private JPanel      navegacion          = new JPanel();
+    private G3Button    logoutBtn           = new G3Button("CERRAR SESION");
+    private G3Button    backBtn             = new G3Button("INICIO");
 
     private JPanel      panelActual;
-    private JPanel      menu            = new JPanel();
-    private G3Button    asistenciasBtn  = new G3Button("MIS ASISTENCIAS");
+    private JPanel      menu                = new JPanel();
+    private G3Button    asistenciasBtn      = new G3Button("<html>&emsp;&emsp;MIS<br>ASISTENCIAS</html>");
+    private G3Button    estudiantesBtn      = new G3Button("<html>ASISTENCIAS<br>ESTUDIANTES</html>");
 
-    private JPanel      pantalla        = new JPanel();
+    private JPanel      pantalla            = new JPanel();
 
     private G3Label     horarioLbl;
-    private JPanel      horarioPnl      = new JPanel();
+    private JPanel      horarioPnl          = new JPanel();
     private JTable      horario;
 
     private G3Label     historicoLbl;
-    private JPanel      historicoPnl    = new JPanel();
+    private JPanel      historicoPnl        = new JPanel();
     private JTable      historico;
 
-    private G3Button    resumenBtn      = new G3Button("RESUMEN");
-    private JPanel      pantallaResumen = new JPanel();
-
+    private G3Button    resumenBtn          = new G3Button("RESUMEN");
+    private JPanel      pantallaResumen     = new JPanel();
     private G3Label     resumenLbl;
-    private JPanel      resumenPnl      = new JPanel();
+    private JPanel      resumenPnl          = new JPanel();
     private JTable      resumen;
+    
+    private JPanel      pantallaEstudiantes = new JPanel();
+    private G3Label     estudiantesLbl;
+    private JPanel      estudiantesPnl      = new JPanel();
+    private JTable      estudiantes;
+    
+    private G3Button    resumenBtnEst       = new G3Button("RESUMEN");
+    private JPanel      pantallaResumenEst  = new JPanel();
+    private G3Label     resumenEstLbl;
+    private JPanel      resumenEstPnl          = new JPanel();
+    private JTable      resumenEst;
 
-    private G3Button    regresarBtn     = new G3Button("REGRESAR");
+    private G3Button    regresarBtn         = new G3Button("REGRESAR");
+    private G3Button    regresarBtnEst      = new G3Button("REGRESAR");
 
     public TeacherForm(UsuarioDTO us, MainForm p) {
-        usuario = us;
-        docente = new Docente();
-        padre = p;
+        usuario     = us;
+        docente     = new Docente();
+        padre       = p;
         panelActual = menu;
         customizeComponent();
-        logoutBtn.addActionListener ( e -> logout());
-        backBtn.addActionListener(e -> verInicio());
+
+        logoutBtn.addActionListener     ( e -> logout());
+        backBtn.addActionListener       (e -> verInicio());
         asistenciasBtn.addActionListener(e -> verAsistencias());
-        resumenBtn.addActionListener(e -> verResumen());
-        regresarBtn.addActionListener(e -> verAsistencias());
+        resumenBtn.addActionListener    (e -> verResumen());
+        regresarBtn.addActionListener   (e -> verAsistencias());
+        estudiantesBtn.addActionListener(e -> verEstudiantes());
+        resumenBtnEst.addActionListener (e -> verEstResumen());
+        regresarBtnEst.addActionListener(e -> verEstudiantes());
         
         setVisible(true);
     }
@@ -101,13 +117,18 @@ public class TeacherForm extends JFrame{
         cabecera.add(bienvenida, BorderLayout.WEST);
 
         menu.setLayout(new BorderLayout());
-        menu.setBorder(new EmptyBorder(ANCHO / 28, ANCHO / 4, ANCHO / 28, ANCHO / 4));
+        menu.setBorder(new EmptyBorder(ANCHO / 20, ANCHO / 7, ANCHO / 20, ANCHO / 7));
 
         asistenciasBtn.setForeground(Style.COLOR_FONT_LIGHT);
         asistenciasBtn.setBackground(Style.COLOR_ACCENT_SOLID);
         asistenciasBtn.setFont(new Font("Century Gothic", Font.BOLD, (ESCALA * 2) / 3));
 
-        menu.add(asistenciasBtn);
+        estudiantesBtn.setForeground(Style.COLOR_FONT_LIGHT);
+        estudiantesBtn.setBackground(Style.COLOR_ACCENT_SOLID);
+        estudiantesBtn.setFont(new Font("Century Gothic", Font.BOLD, (ESCALA * 2) / 3));
+
+        menu.add(asistenciasBtn, BorderLayout.WEST);
+        menu.add(estudiantesBtn, BorderLayout.EAST);
 
         pantalla.setLayout(null);
         
@@ -131,7 +152,6 @@ public class TeacherForm extends JFrame{
         pantalla.add(historicoLbl);
         pantalla.add(historicoPnl);
 
-        pantallaResumen.removeAll();
         pantallaResumen.setLayout(null);
 
         resumenLbl = new G3Label("RESUMEN", new Rectangle(SEPARACION, SEPARACION, ANCHO - 3 * SEPARACION, ALTO / 10));
@@ -144,10 +164,44 @@ public class TeacherForm extends JFrame{
         resumenLbl.add(regresarBtn);
 
         resumenPnl.setBounds(SEPARACION, ALTO / 10 + 2 * SEPARACION, ANCHO - (3 * SEPARACION), ESCALA * 2);
-        crearResumen();
+        crearEstudiantes();
 
         pantallaResumen.add(resumenLbl);
         pantallaResumen.add(resumenPnl);
+
+        pantallaEstudiantes.setLayout(null);
+
+        estudiantesLbl = new G3Label("ASISTENCIAS ESTUDIANTES", new Rectangle(SEPARACION, SEPARACION, ANCHO - 3 * SEPARACION, ALTO / 10));
+
+        resumenBtnEst.setBounds(ANCHO - 3 * SEPARACION - ANCHO / 7 - (SEPARACION * 2) / 3, (SEPARACION * 2) / 3, ANCHO / 7, ALTO / 10 - (SEPARACION * 4) /3);
+        resumenBtnEst.setFont(new Font("Century Gothic", Font.BOLD, (ESCALA) / 4));
+        resumenBtnEst.setBackground(Style.COLOR_FONT_LIGHT);
+        resumenBtnEst.setForeground(Style.COLOR_ACCENT_SOLID);
+        resumenBtnEst.setOpaque(true);
+        estudiantesLbl.add(resumenBtnEst);
+
+        estudiantesPnl.setBounds(SEPARACION, ALTO / 10 + 2 * SEPARACION, ANCHO - (3 * SEPARACION), ESCALA * 2);
+        crearResumen();
+
+        pantallaEstudiantes.add(estudiantesLbl);
+        pantallaEstudiantes.add(estudiantesPnl);
+
+        pantallaResumenEst.setLayout(null);
+
+        resumenEstLbl = new G3Label("RESUMEN", new Rectangle(SEPARACION, SEPARACION, ANCHO - 3 * SEPARACION, ALTO / 10));
+
+        regresarBtnEst.setBounds(ANCHO - 3 * SEPARACION - ANCHO / 7 - (SEPARACION * 2) / 3, (SEPARACION * 2) / 3, ANCHO / 7, ALTO / 10 - (SEPARACION * 4) /3);
+        regresarBtnEst.setFont(new Font("Century Gothic", Font.BOLD, (ESCALA) / 4));
+        regresarBtnEst.setBackground(Style.COLOR_FONT_LIGHT);
+        regresarBtnEst.setForeground(Style.COLOR_ACCENT_SOLID);
+        regresarBtnEst.setOpaque(true);
+        resumenEstLbl.add(regresarBtnEst);
+
+        resumenEstPnl.setBounds(SEPARACION, ALTO / 10 + 2 * SEPARACION, ANCHO - (3 * SEPARACION), ESCALA * 2);
+        crearResumenEst();
+
+        pantallaResumenEst.add(resumenEstLbl);
+        pantallaResumenEst.add(resumenEstPnl);
 
         add(cabecera, BorderLayout.NORTH);
         add(panelActual);
@@ -156,10 +210,11 @@ public class TeacherForm extends JFrame{
         horarioPnl.removeAll();
         String[] header = {"Hora", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"};
         Object[][] data = new Object[6][6];
-        
+        /*
+         * Aqui va el metodo para poner el horario en Object [][] data
+         */
         horario = new JTable(data, header);
         horario.setShowHorizontalLines(true);
-        //horario.setShowVerticalLines(true);
         horario.setGridColor(Style.COLOR_FONT);
         horario.setBackground(Style.COLOR_FONT_LIGHT);
         horario.setForeground(Style.COLOR_ACCENT_SOLID);
@@ -175,10 +230,11 @@ public class TeacherForm extends JFrame{
         historicoPnl.removeAll();
         String[] header = {"Fecha", "Hora", "Materia", "Asistencia"};
         Object[][] data = new Object[12][4];
-
+        /*
+         * Aqui va el metodo para poner todas las asistencias (Ausente 0, Atrasado 1, Presente 2) del usuario en Object [][] data
+         */
         historico = new JTable(data, header);
         historico.setShowHorizontalLines(true);
-        //historico.setShowVerticalLines(true);
         historico.setGridColor(Style.COLOR_FONT);
         historico.setRowSelectionAllowed(true);
         historico.setColumnSelectionAllowed(false);
@@ -192,10 +248,11 @@ public class TeacherForm extends JFrame{
         resumenPnl.removeAll();
         String[] header = {"Materia", "Asistencias", "Atrasos", "Inasistencias"};
         Object[][] data = new Object[6][4];
-
+        /*
+         * Aqui va el metodo para poner el total de asistencias, atrasos e inasistencias del usuario en Object [][] data
+         */
         resumen = new JTable(data, header);
         resumen.setShowHorizontalLines(true);
-        //resumen.setShowVerticalLines(true);
         resumen.setGridColor(Style.COLOR_FONT);
         resumen.setRowSelectionAllowed(true);
         resumen.setColumnSelectionAllowed(false);
@@ -204,6 +261,42 @@ public class TeacherForm extends JFrame{
         resumen.setFillsViewportHeight(true);
 
         resumenPnl.add(new JScrollPane(resumen));
+    }
+    private void crearEstudiantes(){
+        estudiantesPnl.removeAll();
+        String[] header = {"Fecha", "Hora", "Materia", "Estudiante", "Asistencia"};
+        Object[][] data = new Object[12][5];
+        /*
+         * Aqui va el metodo para poner todas las asistencias (Ausente 0, Atrasado 1, Presente 2) de los estudiantes en Object [][] data
+         */
+        estudiantes = new JTable(data, header);
+        estudiantes.setShowHorizontalLines(true);
+        estudiantes.setGridColor(Style.COLOR_FONT);
+        estudiantes.setRowSelectionAllowed(true);
+        estudiantes.setColumnSelectionAllowed(false);
+
+        estudiantes.setPreferredScrollableViewportSize(new Dimension(ANCHO - 3 * SEPARACION, ESCALA * 2));
+        estudiantes.setFillsViewportHeight(true);
+
+        estudiantesPnl.add(new JScrollPane(estudiantes));
+    }
+    private void crearResumenEst(){
+        resumenPnl.removeAll();
+        String[] header = {"Estudiante", "Materia", "Asistencias", "Atrasos", "Inasistencias"};
+        Object[][] data = new Object[12][5];
+        /*
+         * Aqui va el metodo para poner el total de asistencias, atrasos e inasistencias de los estudiantes en Object [][] data
+         */
+        resumenEst = new JTable(data, header);
+        resumenEst.setShowHorizontalLines(true);
+        resumenEst.setGridColor(Style.COLOR_FONT);
+        resumenEst.setRowSelectionAllowed(true);
+        resumenEst.setColumnSelectionAllowed(false);
+
+        resumenEst.setPreferredScrollableViewportSize(new Dimension(ANCHO - 3 * SEPARACION, ESCALA * 2));
+        resumenEst.setFillsViewportHeight(true);
+
+        resumenEstPnl.add(new JScrollPane(resumenEst));
     }
 
     private void logout(){
@@ -222,9 +315,21 @@ public class TeacherForm extends JFrame{
         customizeComponent();
     }
     private void verResumen(){
-        pantalla.removeAll();
+        pantallaResumen.removeAll();
         remove(panelActual);
         panelActual = pantallaResumen;
+        customizeComponent();
+    }
+    private void verEstudiantes(){
+        pantallaEstudiantes.removeAll();
+        remove(panelActual);
+        panelActual = pantallaEstudiantes;
+        customizeComponent();
+    }
+    private void verEstResumen(){
+        pantallaResumenEst.removeAll();
+        remove(panelActual);
+        panelActual = pantallaResumenEst;
         customizeComponent();
     }
 }
