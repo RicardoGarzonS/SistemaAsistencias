9e022888c2aff09962715ef4b92dbdb45115b695
Amsi <sebas.jibajava@gmail.com>
2025-02-10 13:41:18 -0500

    loginbreak

--- src/UserInterface/Form/LogInForm.java
+++ src/UserInterface/Form/LogInForm.java
@@ -167,6 +167,7 @@ public class LogInForm extends JFrame{
                         break;
                     case 2:
                         padre.mostrarProfesor(us);
+                        break;
                     case 3:
                         padre.mostrarAdmin(us);
                         break;
